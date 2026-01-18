aaee5cca790b7d47d0c8142591ebb594eed0b68b
ioana <ioanapana2003@gmail.com>
2026-01-18 16:23:09 +0200

    first commit

---
+++ src/main/java/ro/ase/ppoo/abstracts/Event.java
@@ -0,0 +1,5 @@
+package ro.ase.ppoo.abstracts;
+
+public abstract class Event {
+
+}
---
+++ src/main/java/ro/ase/ppoo/program/Main.java
@@ -0,0 +1,7 @@
+package ro.ase.ppoo.program;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
